8ff6c79b8c08395742e08182ffa871c71109095a
SHAFIEEJAD <95031820+SHAFIEEJAD@users.noreply.github.com>
2023-09-28 12:57:11 +0200

    first commit

---
+++ resources/js/Pages/create.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Hey There!</h1>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return inertia('create');
+});
+Route::get('/control-panel', function () {
+    return inertia('admin/index');
+});
+Route::get('/control-panel/user/create', function () {
+    return inertia('admin/user/create');
+});
+// Route::get('/', function () {
+//     return inertia('create');
+// });
+// Route::get('/', function () {
+//     return inertia('create');
+// });
+// Route::get('/', function () {
+//     return inertia('create');
+// });
+// Route::get('/', function () {
+//     return inertia('create');
+// });
